9ba9889ad1fb83f732d019de2a86ab206185669e
TranThiHuyenTrang <trang.tran21@student.passerellesnumeriques.org>
2022-03-28 23:47:47 +0700

    Dat 28/3

---
+++ Models/cartModel.php
@@ -0,0 +1,39 @@
+<?php 
+    class cartModel extends Model {
+        
+        function getCartListByCustomerId($id){
+            $results = $this->con->query(
+                "SELECT link, name, price
+                FROM carts
+                LEFT JOIN products
+                ON carts.productId = products.id
+                LEFT JOIN imgs
+                ON products.id = imgs.productId
+                LEFT JOIN customers
+                ON carts.cusId = customers.id
+                WHERE customers.id = {$id}
+            ");
+            $cartList = [];
+            while ($row = $results->fetch_assoc()) {
+                array_push($cartList, $row);
+            }
+            return $cartList;
+        }
+
+        function addCart($cusId, $productId, $quantity){
+            $sql = "INSERT INTO carts (cusId, productId, quantity) 
+            VALUES('$cusId', '$productId', '$quantity')";
+            return $this->con->query($sql);
+        }
+
+        function updateCart($id, $quantity){
+            $sql = "UPDATE carts SET quantity = '$quantity' WHERE id ={$id}";
+            return $this->con->query($sql);
+        }
+
+        function deleteCart($id){
+            $sql = "DELETE FROM carts WHERE id ={$id}";
+            return $this->con->query($sql);
+        }
+    }
+?>
