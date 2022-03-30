84338fd360167ff81789e79909eaf11da74d83b3
HanhVQQD <100430789+HanhVQQD@users.noreply.github.com>
2022-03-30 15:53:04 +0700

    Add files via upload

---
+++ Views/Pages/reviews.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./cs/reviewas.css">
+    <title>Đánh giá</title>
+</head>
+
+<body>
+    <section class="h-content">
+        <h1>Hiếu Huynh và Khách hàng</h1>
+        <div class="h-soLieu">
+            <div class="h-chonDung">
+                <h2>1923</h2>
+                <p>người đã chọn dùng</p>
+            </div>
+            <div class="h-slSpBan">
+                <h2>25611</h2>
+                <p>sản phẩm được bán cho khách hàng</p>
+            </div>
+
+            <div class="h-khachHangMua">
+                <h2>25%</h2>
+                <p>khách hàng mua từ 5 lần trở lên</p>
+            </div>
+
+            <div class="h-khBinhLuan">
+                <h2>37500</h2>
+                <p>lời đánh giá và bình luận tốt</p>
+            </div>
+        </div>
+        <div class="h-binhLuanCuaKh">
+            <div class="h-card">
+                <img id="h-img" src="https://event.mediacdn.vn/2020/9/29/my-tam-p-16013539097921191329031.png" alt="Avatar">
+                <div class="h-container">
+                    <h4><b>John Doe</b></h4>
+                    <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click or double click me to add your own content and make changes to the font.</p>
+                </div>
+            </div>
+
+            <div class="h-card">
+                <img id="h-img" src="https://kenh14cdn.com/2018/5/23/695fce3cefc5fa44d7b1ee489a7eedfd1509935350-1527058288861254197560.jpg" alt="Avatar">
+                <div class="h-container">
+                    <h4><b>John Doe</b></h4>
+                    <p>“ I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click or double click me to add your own content and make changes to the font.</p>
+                </div>
+            </div>
+
+            <div class="h-card">
+                <img id="h-img" src="https://vcdn-ngoisao.vnecdn.net/2021/06/10/Re-Bao-Thanh-2-9059-1623302203.jpg" alt="Avatar">
+                <div class="h-container">
+                    <h4><b>John Doe</b></h4>
+                    <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click or double click me to add your own content and make changes to the font.</p>
+                </div>
+            </div>
+
+            <div class="h-card">
+                <img id="h-img" src="https://image.thanhnien.vn/w660/Uploaded/2022/wsxrxqeiod/2020_07_31/dp02-anh-noi-bat_oklq.jpg" alt="Avatar">
+                <div class="h-container">
+                    <h4><b>John Doe</b></h4>
+                    <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click or double click me to add your own content and make changes to the font.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
