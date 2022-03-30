932d5de7f1b3f4d2eb12b15184859cc245c8631d
Leennguyen <nguyenthilinh26112002@gmail.com>
2022-03-30 15:37:56 +0700

    Linh update header-footer 30/3

---
+++ Views/Pages/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+</head>
+<body>
+    <section class="header">
+        <!-- <div></div> -->
+        <div class="header-logo">
+            <img class="header-logo__img" src="asset/img/Logo.png" alt="">
+        </div>
+        <ul class="header-menu">
+            <li><a href="homePage.php">TRANG CHỦ</a></li>
+            <li><a href="aboutUs.php">VỀ CHÚNG TÔI</a></li>
+            <li><a href="products.php">SẢN PHẨM</a></li>
+            <li><a href="stories.php">CÂU CHUYỆN</a></li>
+            <li><a href="review.php">ĐÁNH GIÁ</a></li>
+            <li><a href="qa.php">HỎI ĐÁP</a></li>
+            <li><a href="contact.php">LIÊN HỆ</a></li>
+        </ul>
+        <div>
+            <div class="header__btn">
+                <input type="text" placeholder="Tìm kiếm...">
+                <i class="fas fa-shopping-cart header-icon__btn"></i>
+                <i class="fa-solid fa-user header-icon__btn"></i>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
+<link rel="stylesheet" href="./asset/css/style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
