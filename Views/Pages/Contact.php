70662347be20b620dae3394b6314b406b0d8ca46
TNguyenk2 <nguyen.nguyen23@student.passerellesnumeriques.org>
2022-04-05 15:16:39 +0700

    TNguyen update CSS & PHP 05/04/2022

---
+++ Views/Pages/Contact.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/Contact.css">
+    <title>Contact</title>
+</head>
+
+<body>
+    <section class="N-container">
+        <div class="N-layout">
+            <div class="N-map"><iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3833.573867606748!2d108.2414556153612!3d16.08758804321766!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314217f3807e8a07%3A0x9631e77f9843d8d2!2zNDAgxJAuIFRy4bqnbiBEdXkgQ2hp4bq_biwgTcOibiBUaMOhaSwgU8ahbiBUcsOgLCDEkMOgIE7hurVuZywgVmlldG5hbQ!5e0!3m2!1sen!2s!4v1648747127271!5m2!1sen!2s"
+                    style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe></div>
+            <div class="N-formContact">
+                <form action="" method="post">
+                    <input class="N-field" type="text" placeholder="Họ và Tên *">
+                    <input class="N-field" type="text" placeholder="Email *">
+                    <input class="N-field" type="text" placeholder="Điện thoại *">
+                    <textarea class="N-field" name="" id="" cols="30" rows="10" placeholder="Lời nhắn *"></textarea>
+                    <input class="N-submit" type="submit" value="Gửi">
+                </form>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
---
+++ Views/Pages/Morestories.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="" href="../CSS/Morestories.css">
+    <title>See more stories</title>
+</head>
+
+<body>
+    <section class="N-container">
+        <div class="N-title">
+            <p>Câu chuyện mỗi ngày</p>
+        </div>
+        <div class="N-layout">
+            <div class="N-layoutImgItem">
+                <img class="N-img" src="https://i.pinimg.com/originals/65/da/d7/65dad7d385e399129e8d2f887e7ce472.webp"
+                    alt="">
+            </div>
+            <div class="N-layoutImgItem">
+                <img class="N-img" src="https://i.pinimg.com/originals/65/da/d7/65dad7d385e399129e8d2f887e7ce472.webp"
+                    alt="">
+            </div>
+            <div class="N-layoutTextItem">
+                <p class="N-textTitle">Chả phụng</p>
+                <p class="N-textContent">Ngày mai sẽ ra sao............
+                    Vận dụng những kiến thức đã học về cách viết câu,
+                    viết đoạn văn hoàn chỉnh, em hãy thực hành viết một đoạn văn ngắn về gia đình em, từ 3 đến 5 câu để
+                    ôn luyện và nâng cao hơn nữa kĩ năng xây dựng đoạn văn sao cho ngắn gọn, dễ hiểu, rành mạch và thu
+                    hút người đọc.
+                    Vận dụng những kiến thức đã học về cách viết câu,
+                    viết đoạn văn hoàn chỉnh, em hãy thực hành viết một đoạn văn ngắn về gia đình em, từ 3 đến 5 câu để
+                    ôn luyện và nâng cao hơn nữa kĩ năng xây dựng đoạn văn sao cho ngắn gọn, dễ hiểu, rành mạch và thu
+                    hút người đọc.Vận dụng những kiến thức đã học về cách viết câu,
+                    viết đoạn văn hoàn chỉnh, em hãy thực hành viết một đoạn văn ngắn về gia đình em, từ 3 đến 5 câu để
+                    ôn luyện và nâng cao hơn nữa kĩ năng xây dựng đoạn văn sao cho ngắn gọn, dễ hiểu, rành mạch và thu
+                    hút người đọc. moojt von mvien ajifs najfh tk ajak tkj a tj kfsj lkr jk ojoaj vjdotjkao vj ajfok
+                    jasojf o j jfij sfjsoj sfjsfn nsjk fnsjk nvjs nsj fnsjfnjk ns nfksnfjk sncsjk nfjkn kcn jkfnjk njn
+                    kaf fnklfj a,dfm.a gnjaf m jnawfkfn nkaj klmfklsnfkl naknjka fjb asjk</p>
+            </div>
+            <div class="N-layoutTextItem">
+                <p class="N-textTitle">Chả phụng</p>
+                <p class="N-textContent">Ngày mai sẽ ra sao............ Vận dụng những kiến thức đã học về cách viết
+                    câu,
+                    viết đoạn văn hoàn chỉnh, em hãy thực hành viết một đoạn văn ngắn về gia đình em, từ 3 đến 5 câu để
+                    ôn luyện và nâng cao hơn nữa kĩ năng xây dựng đoạn văn sao cho ngắn gọn, dễ hiểu, rành mạch và thu
+                    hút người đọc.Vận dụng những kiến thức đã học về cách viết câu,
+                    viết đoạn văn hoàn chỉnh, em hãy thực hành viết một đoạn văn ngắn về gia đình em, từ 3 đến 5 câu để
+                    ôn luyện và nâng cao hơn nữa kĩ năng xây dựng đoạn văn sao cho ngắn gọn, dễ hiểu, rành mạch và thu
+                    hút người đọc. moojt von mvien ajifs najfh tk ajak tkj a tj kfsj lkr jk ojoaj vjdotjkao vj ajfok
+                    jasojf o j jfij sfjsoj sfjsfn nsjk fnsjk nvjs nsj fnsjfnjk ns nfksnfjk sncsjk nfjkn kcn jkfnjk njn
+                    kaf fnklfj a,dfm.a gnjaf m jnawfkfn nkaj klmfklsnfkl naknjka fjb asjk</p>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
