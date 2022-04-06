76202126ba83df8c5ba8e98f0432fc26b8b20585
Thien Dam <thiendam052@gmail.com>
2022-04-07 00:52:44 +0700

    Thien update

--- Views/Pages/Home.php
+++ Views/Pages/Home.php
@@ -29,19 +29,5 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"
     integrity="sha512-XtmMtDEcNz2j7ekrtHvOVR4iwwaD6o/FUJe6+Zq+HgcCsk3kj4uSQQR8weQ2QVj1o0Pk6PwYLohm206ZzNfubg=="
     crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-<script>
-var slide = $('.slides').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    arrows: true,
-});
-$(".slides").on("afterChange", (e, slick, index) => {
-    $(".title").css("opacity", 1)
-    if (index == 0) {
-        $(".title").css("opacity", 0);
-    }
-})
-</script>
+<script src="<?=ROOT_URL?>Assets/js/Home.js"></script>
+<link rel=" stylesheet" href="<?=ROOT_URL?>Assets/css/HomeStyle.css">
