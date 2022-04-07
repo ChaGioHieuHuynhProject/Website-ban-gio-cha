033d5b1958907e168e75d1fb410f36ec23a99d64
NDK2509 <dangkyqt2509@gmail.com>
2022-04-07 16:04:30 +0700

    Ky commit

--- Core/Functions.php
+++ Core/Functions.php
@@ -1,9 +1,11 @@
-<?php 
-    define("ROOT_URL", "http://localhost:8080/Website-ban-gio-cha/");
-    function Error() {
+<?php
+    define("ROOT_URL", "http://localhost/KyNguyen/Website-ban-gio-cha/");
+    function Error()
+    {
         require_once "error.php";
     }
-    function Redirect($controller, $action = "") {
-        return "/Website-ban-gio-cha/$controller/$action";
+    function Redirect($controller, $action = "")
+    {
+        return "/KyNguyen/Website-ban-gio-cha/$controller/$action";
     }
-  
+?>
