19680be682ba2455eb838a0c05dfe2c1b25e8840
NDK2509 <dangkyqt2509@gmail.com>
2022-04-09 17:45:22 +0700

    Ky edit something 9/4

--- Views/MainLayout.php
+++ Views/MainLayout.php
@@ -39,11 +39,16 @@
                     </a>
                 <?php } else { ?>
                     <span class="logedIn-nav">
-                        <span><?= $_SESSION["LOGIN"]["cusName"] ?></span>
+                        <span><?= $_SESSION["LOGIN"]["firstName"] ?></span>
+                        <span>
+                            <svg class="header-icon__btn" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                                <path d="M192 384c-8.188 0-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L192 306.8l137.4-137.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-160 160C208.4 380.9 200.2 384 192 384z" />
+                            </svg>
+                        </span>
                         <ul class="user-dropdown">
                             <li>Thông tin cá nhân</li>
                             <li>Lịch sử mua hàng</li>
-                            <li><a href="<?=Redirect("Home", "Logout")?>">Đăng xuất</a></li>
+                            <li><a href="<?= Redirect("Home", "Logout") ?>">Đăng xuất</a></li>
                         </ul>
                     </span>
                 <?php } ?>
@@ -54,7 +59,6 @@
     <footer class="footer">
         <div class="footer-top">
             <img class="footer-top__img" src="<?= ROOT_URL ?>Assets/img/Logo.png" alt="">
-            <div></div>
             <div class="footer-top-right">
                 <span style="font-weight:bold">Theo dõi chúng tôi tại</span>
                 <a href="">
--- Views/Pages/Product.php
+++ Views/Pages/Product.php
@@ -3,46 +3,40 @@
     <div class="content">
         <div class="div-image">
             <img src="<?= ROOT_URL ?>Assets/img/Banner2.png" alt="" />
-            <p class="title">Chả lụa</p>
+            <p class="title button"><a>Chả lụa</a></p>
             <div class="overlay"></div>
-            <div class="button"><a href=""> Xem Thêm </a></div>
             <div class="block"></div>
         </div>
         <div class="div-image">
             <img src="<?= ROOT_URL ?>Assets/img/Banner2.png" alt="" />
-            <p class="title">Chả lụa</p>
+            <p class="title button"><a>Chả lụa</a></p>
             <div class="overlay"></div>
-            <div class="button"><a href=""> Xem Thêm </a></div>
             <div class="block"></div>
         </div>
         <div class="div-image">
             <img src="<?= ROOT_URL ?>Assets/img/Banner2.png" alt="" />
-            <p class="title">Chả lụa</p>
+            <p class="title button"><a>Chả lụa</a></p>
             <div class="overlay"></div>
-            <div class="button"><a href=""> Xem Thêm </a></div>
             <div class="block"></div>
         </div>
         <div class="div-image">
             <img src="<?= ROOT_URL ?>Assets/img/Banner2.png" alt="" />
-            <p class="title">Chả lụa</p>
+            <p class="title button"><a>Chả lụa</a></p>
             <div class="overlay"></div>
-            <div class="button"><a href=""> Xem Thêm </a></div>
             <div class="block"></div>
         </div>
         <div class="div-image">
             <img src="<?= ROOT_URL ?>Assets/img/Banner2.png" alt="" />
-            <p class="title">Chả lụa</p>
+            <p class="title button"><a>Chả lụa</a></p>
             <div class="overlay"></div>
-            <div class="button"><a href=""> Xem Thêm </a></div>
             <div class="block"></div>
         </div>
         <div class="div-image">
             <img src="<?= ROOT_URL ?>Assets/img/Banner2.png" alt="" />
-            <p class="title">Chả lụa</p>
+            <p class="title button"><a>Chả lụa</a></p>
             <div class="overlay"></div>
-            <div class="button"><a href=""> Xem Thêm </a></div>
             <div class="block"></div>
         </div>
     </div>
 </section>
-<link rel="stylesheet" href="<?=ROOT_URL?>Assets/css/product.css">
+<link rel="stylesheet" href="<?= ROOT_URL ?>Assets/css/product.css">
